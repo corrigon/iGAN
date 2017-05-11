5ecf18d9b6190517f43ed4a2963a7b24972d9dd6
Ubuntu <ubuntu@ip-10-170-144-69.ec2.internal>
2017-05-11 13:22:45 +0000

    added sort bu aspect

---
+++ sort_img_by_aspect.py
@@ -0,0 +1,47 @@
+################### sort images by aspect ################
+from glob import glob
+from scipy.misc import imread, imsave, imresize
+from model_def.dcgan_theano import Model
+import numpy as np
+import theano
+import theano.tensor as T
+from lib.theano_utils import floatX
+from lib.rng import np_rng
+import os
+import shutil
+
+#Load model
+aa = Model(model_name='handbag_64', model_file='models/handbag_64.dcgan_theano')
+
+#compile f_enc
+x_s = T.ftensor4('imgs')
+z_tilda_sym = aa.model_P(x_s)
+f_enc = theano.function([x_s],z_tilda_sym)
+
+paths = glob('/home/ubuntu/Data/hand_bags/*.jpg')[:1000]
+z_arr = []
+for path in paths:    
+    print path
+    img = imread(path)
+    if img.ndim == 2:
+        img = np.repeat(img[:,:,None],3,2)        
+    img = imresize(img,(64,64))[None,...]
+    t_img = aa.transform(img)
+    z = f_enc(t_img)
+    z_arr.append(z)
+    shutil.copy(path, 'our_recon/%s'%os.path.basename(path))
+
+z = np.vstack(z_arr) #1000x100
+paths = np.asarray(paths)
+for i in range(10):
+    col_feats = z[:,i]
+    idxs = np.argsort(col_feats)
+    print 'aspect %d'%i
+    base_dir = 'aspect_sorted/%d'%i
+    os.mkdir(base_dir)
+    for k,j in enumerate(idxs):        
+        shutil.copy(paths[j], '%s/%03d_%s'%(base_dir, k, os.path.basename(paths[j])))
+
+
+
+
